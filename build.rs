08ba1cb3b9aa6c93e6bea3c2cab411a8559cae97
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-01-09 19:25:20 +0100

    Make cc -std flag only applied if supported to get rid of warning on windows

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     cc::Build::new()
         .file("fast_transport/EMD_wrapper.cpp")
         .cpp(true)
-        .flag("-std=c++14")
+        .flag_if_supported("-std=c++14")
         .compile("fast_transport");
 
     let bindings = bindgen::Builder::default()
